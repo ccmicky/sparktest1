3b2658523f9ab0c33c673059a083e9ceecfc9a15
hadoop <ccheng@zmjiudian.com>
2015-10-28 15:04:13 +0800

    RelWordEntity.java

---
+++ test1/src/com/test/RelWordEntity.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2015/10/19.
+ */
+public class RelWordEntity {
+    public int IDX = 0;
+    public int KeyWordID = 0;
+    public String RelWord = "";
+    public String RelWordPOS = "";
+    public int State = 0;
+    public Date CreateTime = new Date();
+    public int Total = 0;
+}
