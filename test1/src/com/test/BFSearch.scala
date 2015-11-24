788422a6b669ced80f071b05f4222d07af0cfb54
hadoop <ccheng@zmjiudian.com>
2015-11-24 12:54:13 +0800

    只搜索直接修饰词

--- test1/src/com/test/BFSearch.scala
+++ test1/src/com/test/BFSearch.scala
@@ -1,5 +1,6 @@
 package com.test
 
+import com.test.Entity.RelItem
 import org.apache.spark.broadcast.Broadcast
 
 /**
@@ -8,33 +9,56 @@ import org.apache.spark.broadcast.Broadcast
 
 object BFSearch {
   def isTagedbyKey(
-                    keys:Broadcast[Array[String]],
-                    values:Broadcast[Array[String]],
-                    tagWord:Map[String,String]) :Boolean = {
+                    keys:Array[String],
+                    values:Array[String],
+                    RelList:List[RelItem]) :Boolean = {
+    var mapitem : scala.collection.mutable.Map[String,String] = scala.collection.mutable.Map()
+    for (rel <- RelList)
+    {
+      if(mapitem.contains(rel.Word1))
+      {
+        mapitem(rel.Word1) += " "+rel.Word2
+      }
+      else
+      {
+        mapitem += (rel.Word1 -> rel.Word2)
+      }
+    }
     //val keys = sc.textFile(keywordfilePath)
     //val values = sc.textFile(valuewordfilePath)
-    keys.value.foreach{ key =>
-      values.value.foreach{value =>
-        val isvisitedlist: List[String] = List()
+    keys.foreach{ key =>
+      values.foreach{value =>
+        var isvisitedlist: List[String] = List()
         var klist: List[String] = List(key.trim)
         while (!klist.isEmpty) {
           val keyword = klist.head
           klist = klist.drop(1)
           if (!isvisitedlist.contains(keyword))
           {
-            isvisitedlist.::(keyword)
-            if (tagWord.contains(keyword)) {
-
-              if (tagWord(keyword).contains(value.trim)) {
+            isvisitedlist = isvisitedlist.::(keyword)
+            if (mapitem.contains(keyword)) {
+              val taglist = mapitem(keyword).split(" ")
+              if (taglist.contains(value.trim)){
                 return true
               }
-              else {
-                value.split(" ").foreach(s => klist.::(s))
-              }
+              taglist.foreach(s => klist=klist.::(s))
             }
           }
         }
       }
+    }
+    return false
+  }
+
+  def ifcontainsKeyword(
+                         keys:Array[String],
+                         tagWord:List[RelItem]) :Boolean = {
+    //val keys = sc.textFile(keywordfilePath)
+    //val values = sc.textFile(valuewordfilePath)
+    val result = tagWord.filter(p => keys.contains(p.Word1) ||keys.contains(p.Word2))
+    if (result.length >0 )
+    {
+      return true
     }
     return false
   }
