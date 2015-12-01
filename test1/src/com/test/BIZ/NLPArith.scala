3afe1730628daf41177df1fadb7946c1c39990f6
hadoop <ccheng@zmjiudian.com>
2015-12-01 15:55:18 +0800

    表达式解析"!"

--- test1/src/com/test/BIZ/NLPArith.scala
+++ test1/src/com/test/BIZ/NLPArith.scala
@@ -2,6 +2,7 @@ package com.test.BIZ
 
 
 import com.test.Entity.ShortSentence
+import spire.std.boolean
 
 import scala.util.parsing.combinator.JavaTokenParsers
 
@@ -24,7 +25,7 @@ object NLPArith {
 
     case Variable(x) =>IncludeWord(x,ss)
     case Variable2(x1,x2) =>IncludeWord2(x1,x2,ss)
-    case UnaryOp("!", x) =>  evaluate(x, ss)
+    case UnaryOp("!", x) =>  ! evaluate(x, ss)
     case UnaryOp("->", x) =>  evaluate(x, ss)
     case BinaryOp("||", x1, x2) =>evaluate(x1, ss) || evaluate(x2, ss)
     case BinaryOp("&&", x1, x2) =>evaluate(x1, ss)  && evaluate(x2, ss)
@@ -42,6 +43,7 @@ object NLPArith {
   // ->
   def IncludeWord(wordlist:String, ss:ShortSentence):Boolean=
   {
+    return false
      val wl = GenRuleWordList(wordlist)
     for( i <- ss.RelList)
     {
@@ -58,6 +60,7 @@ object NLPArith {
 
   def IncludeWord2(wordlist1:String,wordlist2:String, ss:ShortSentence):Boolean=
   {
+    return false
     val wl1 = GenRuleWordList(wordlist1)
     val wl2 =  GenRuleWordList(wordlist2)
     for( i <- ss.RelList)
@@ -86,39 +89,41 @@ object NLPArith {
     }
 
     // now simplify at the top, assuming the components are already simplified
-    def simplifyTop(x: Expr) = x match {
-      // Double negation returns the original value
-      case UnaryOp("-", UnaryOp("-", x)) => x
+    def simplifyTop(x: Expr) :Expr = {
+      x match {
+        // Double negation returns the original value
+        case UnaryOp("-", UnaryOp("-", x)) => x
 
-      // Positive returns the original value
-      case UnaryOp("+", x) => x
+        // Positive returns the original value
+        case UnaryOp("+", x) => x
 
-      // Multiplying x by 1 returns the original value
-      case BinaryOp("**", x, Number("1")) => x
+        // Multiplying x by 1 returns the original value
+        case BinaryOp("**", x, Number("1")) => x
 
-      // Multiplying 1 by x returns the original value
-      case BinaryOp("**", Number("1"), x) => x
+        // Multiplying 1 by x returns the original value
+        case BinaryOp("**", Number("1"), x) => x
 
-      // Multiplying x by 0 returns zero
-      case BinaryOp("**", x, Number("0")) => Number("0")
+        // Multiplying x by 0 returns zero
+        case BinaryOp("**", x, Number("0")) => Number("0")
 
-      // Multiplying 0 by x returns zero
-      case BinaryOp("**", Number("0"), x) => Number("0")
+        // Multiplying 0 by x returns zero
+        case BinaryOp("**", Number("0"), x) => Number("0")
 
-      // Dividing x by 1 returns the original value
-      case BinaryOp("/", x, Number("1")) => x
+        // Dividing x by 1 returns the original value
+        case BinaryOp("/", x, Number("1")) => x
 
-      // Dividing x by x returns 1
-      case BinaryOp("/", x1, x2) if x1 == x2 => Number("1")
+        // Dividing x by x returns 1
+        case BinaryOp("/", x1, x2) if x1 == x2 => Number("1")
 
-      // Adding x to 0 returns the original value
-      case BinaryOp("+", x, Number("0")) => x
+        // Adding x to 0 returns the original value
+        case BinaryOp("+", x, Number("0")) => x
 
-      // Adding 0 to x returns the original value
-      case BinaryOp("+", Number("0"), x) => x
+        // Adding 0 to x returns the original value
+        case BinaryOp("+", Number("0"), x) => x
 
-      // Anything else cannot (yet) be simplified
-      case e => e
+        // Anything else cannot (yet) be simplified
+        case e => e
+      }
     }
 
     simplifyTop(simpSubs)
@@ -127,27 +132,46 @@ object NLPArith {
 
 object Arith extends JavaTokenParsers {
   def compose(bi: Expr, list: List[String ~ Expr]): Expr = list match {
-    case Nil => bi
-    case (op ~ e) :: t => compose(BinaryOp(op, bi, e), t)
+    case Nil => {
+      bi}
+    case (op ~ e) :: t =>{
+      compose(BinaryOp(op, bi, e), t)
+    }
   }
 
-  def expr: Parser[Expr] = term ~ rep("+" ~ term | "-" ~ term| "||" ~ term) ^^ {
+  def decompose(list: Option[String] ~ Expr): Expr = list match {
+    case Some(v) ~ f =>{
+      UnaryOp("!", f)}
+    case None ~ f =>{f}
+  }
+
+  def expr: Parser[Expr] = term ~ rep("||" ~ term) ^^ {
+    case t ~ list => {
+      compose(t, list)}
+  }
+
+  def term: Parser[Expr] = nofactor ~ rep( "&&" ~ nofactor ) ^^ {
     case t ~ list => compose(t, list)
   }
 
-  def term: Parser[Expr] = factor ~ rep("*" ~ factor | "/" ~ factor| "&&" ~ factor ) ^^ {
-    case t ~ list => compose(t, list)
-  }
-
+  def nofactor : Parser[Expr] =  opt("!") ~ factor ^^ (x =>
+     decompose(x))
 
   def factor: Parser[Expr] = stringLiteral ~ "->" ~ stringLiteral ^^{
-     x => UnaryOp("->", Variable2(x._1._1,x._2))
-  } | ("-"|"!") ~ stringLiteral ^^ { x1 => UnaryOp(x1._1 , Variable(x1._2)) } |stringLiteral ^^ { x => Variable(x) } |  "(" ~> expr <~ ")"
+     x =>
+       UnaryOp("->", Variable2(x._1._1,x._2))
+  }  |stringLiteral ^^ { x => Variable(x) } |  "(" ~> expr <~ ")"
+
 
   def parse(str: String): Expr = {
     val result = parseAll(expr, str)
     if (result.successful)
-      result.get
+      {
+        println(NLPArith.evaluate(result.get,null))
+        result.get
+      }
+
     else Number("0")
   }
+
 }
